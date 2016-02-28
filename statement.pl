eff563391b5c3800872744ea85df37d04a45acdb
Nikolaos Dymitriadis <nndymitriadis@gmail.com>
2016-02-28 17:50:58 +0100

    minor tweaks

--- statement.pl
+++ statement.pl
@@ -18,12 +18,13 @@ M.epush() := M.put(env, M.env.push()).
 M.epop()  := M.put(env, M.env.pop()).
 
 M.well_typed(Exp) := M :- types(M.env, Exp, _).
+M.well_typed(Exp, Type) := M :- types(M.env, Exp, Type).
 M.expect_type(Type, Exp) := M :- expect_type(M.env, Exp, Type). 
 
 M.merge_return(M1,M2) := M.put(returned, Ret) :-
     M1.returned = true, M2.returned = true -> Ret = true ; Ret = false.
 
-%%%%%
+%%% SIMPLE STATEMENTS %%%
 
 M.correct(skip) := M.
 M.correct(return) := M.put(returned, true) :-
@@ -47,27 +48,29 @@ M.correct(decr(Id)) := M :-
 M.correct(=(Id, Exp)) := M.expect_type(VarInfo.type, Exp) :-
     ( VarInfo = M.env.get_var(Id) -> true ; fail("variable ~w not declared", [Id]) ). 
 
+%%% CONTROL STRUCTURES %%%
+
 M.correct(block(Stmts)) := M.epush().corrects(Stmts).epop().
 
 M.correct(while(true, Do)) := M.epush().correct(Do).epop() :- !.
 M.correct(while(While, Do)) := M.expect_type(boolean, While).epush().correct(Do).epop().put(returned, M.returned).
 
-M.correct(if(true, Then, Else)) := M.epush().correct(Then).epop() :- _ = M.epush().correct(Else), !.
-M.correct(if(false, Then, Else)) := M.epush().correct(Else).epop() :- _ = M.epush().correct(Then), !.
+M.correct(if(true, Then, Else)) := M.epush().correct(Then).epop() :- !, _ = M.epush().correct(Else).
+M.correct(if(false, Then, Else)) := M.epush().correct(Else).epop() :- !, _ = M.epush().correct(Then).
 M.correct(if(If, Then, Else)) := M.expect_type(boolean, If).merge_return(
     M.epush().correct(Then).epop(),
     M.epush().correct(Else).epop()).
 
 M.correct(if(If, Then)) := M.correct(if(If, Then, skip)).
 
-%%% STATEMENTS %%%
+%%% DECLARATIONS %%%
 
-M.correct(decl(_, [])) := M.
-M.correct(decl(Type, [H|T])) := M.decl_correct(Type, H).correct(decl(Type, T)).
+M.correct(decl(Type, Decls)) := M.decls_correct(Type, Decls).
 
+M.decls_correct(_, []) := M.
+M.decls_correct(Type, [H|T]) := M.decl_correct(Type, H).decls_correct(Type, T).
 
 M.decl_correct(Type, init(Id, Exp)) := M.expect_type(Type, Exp).decl_correct(Type, noinit(Id)).
-
 M.decl_correct(Type, noinit(Id)) := M.put(env, M.env.add_var(Id, Type)) :-
     can_shadow(M.env, Id) -> true ; fail("variable ~w already declared", [Id]).
 
