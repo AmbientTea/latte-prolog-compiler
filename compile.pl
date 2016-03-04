47ecea94f3286d3aea695d1248dadff22f55eaa8
Nikolaos Dymitriadis <nndymitriadis@gmail.com>
2016-03-04 20:03:45 +0100

    main program options for future compilation

--- compile.pl
+++ compile.pl
@@ -7,6 +7,8 @@
 :- use_module(frontend).
 :- use_module(eval).
 
+
+
 :- % main
 (
 	OptSpec =
@@ -17,26 +19,38 @@
 		  , longflags(['content'] )
 		  , help([ 'parsed content: exp, prog'])
 		  ]
-		  /*
-		, [ opt(env)
-		  , type(term)
-		  , default(env{})
-		  , shortflags([e])
-		  , longflags(['env'])
-		  , help(['specify environment'])
+		, [ opt(mode)
+		  , type(atom)
+		  , default(eval)
+		  , shortflags([m])
+		  , longflags(['mode'])
+		  , help(['mode: eval, compile'])
 		  ]
-		  */
+		, [ opt(out)
+		  , type(string)
+		  , default("")
+		  , shortflags([o])
+		  , longflags(['out'])
+		  , help(['output file (compilation only)'])
+		  ]
     	],
 	current_prolog_flag(argv, LineArgs),
 	opt_parse(OptSpec, LineArgs, Opts, Args),
 	member(content(Cont), Opts),
+	member(mode(Mode), Opts),
 	
 	(Args = [File | _]       -> true ; fail("no file specified")),
+	( member(out(Out), Opts), Out \= ""
+	; file_name_extension(Base, _, File), file_name_extension(Base, ".out", Out)),
 	(parse(Cont, File, Tree) -> true ; fail("parsing failed") ),
 	(check(Tree)             /*-> true ; fail("type check failed")*/),
-	( Cont = exp -> eval_exp(Tree)
-	; Cont = stmt -> eval_stmt(Tree)
-	; Cont = program -> eval_program(Tree) ),
+	( Mode = eval ->
+	    ( Cont = exp -> eval_exp(Tree)
+	    ; Cont = stmt -> eval_stmt(Tree)
+	    ; Cont = program -> eval_program(Tree) )
+    ; Mode = compile ->
+        (fail("compilation not implemented yet"))
+    ; fail("invalid mode: ~w", [Mode])),
 	halt(0)
 ) ; halt(-1).
 
