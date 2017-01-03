3c8bed39d57a214dd1e9ca6a23262572f3ac18c6
Nikolaos Dymitriadis <nndymitriadis@gmail.com>
2017-01-04 00:35:37 +0100

    started rewriting IR

--- ir2.pl
+++ ir2.pl
@@ -6,14 +6,24 @@
 :- op(600, xfy, '&&').
 :- op(600, xfy, '||').
 
-ir_env(ir2{
+ir_empty_env(ir2{
     ask: _{},
-    create: _{},
     mod: _{},
-    last_block: ...,
-    var_types: _{}
-}).
+    gen: _{},
+    last_block: ...
+}) --> [].
 
+E.add_to(Field, Id, Elem) := E.put(Field, E.Field.put(Id, Elem)).
+
+ir_ask_env(Id, Reg, Env) -->
+    ir_empty_env(Env1),
+    { Env = Env1.put(ask, Env1.ask.put(Id, Reg)) }.
+
+ir_ask_env(Ask, Env) -->
+    ir_empty_env(Env1),
+    { Env = Env1.put(ask, Ask) }.
+
+/*
 E.reset() := E.put(ask, _{}).put(mod, _{}).put(create,_{}).
 E.add_var(Id, Type) := E.put(var_types, E.var_types.put(Id, Type)).
 E.get_var(Id) := E.var_types.Id.
@@ -41,11 +51,26 @@ E.add_mod(Key, Val) := E2 :-
 
 E.add_create(Key, Val) := E.put(create, E.create.put(Key,Val)).
 
+*/
 %%%%%%%%%%%%%%%
 %%% MERGING %%%
 %%%%%%%%%%%%%%%
 
+semicolon_merge( Env1, Env2, Env ) --> {
+    Env = _{
+        ask: Ask,
+        mod: Mod,
+        gen: Gen,
+        last_block: Block
+    },
+    Ask set_is Env1.ask + (Env2.ask ~ Env1.gen ~ Env1.mod),
+    Mod set_is Env2.mod + (Env1.mod - Env2.mod) - Env1.gen,
+    Gen set_is Env1.gen + Env2.gen,
+    Block = Env2.last_block,
+    (Env1.last_block = Env2.last_block, ! ; true)
+}.
 
+/*
 ir_while_merge(PreEnv, PostEnv, NewEnv) -->
     ir_while_merge_mods(PreEnv, PostEnv.last_block, PostEnv.mod, Env1),
     { NewEnv = Env1.add_ask(PostEnv.ask) }
@@ -85,15 +110,20 @@ ir_merge_if_mods(PreEnv, Label1, Mod1, Label2, Mod2, NewEnv) -->
 ir_merge_if_mods(PreEnv, _, _{}, _, _{}, PreEnv) --> [].
 ir_merge_if_mods(PreEnv, Label1, _{}, Label2, Mod2, NewEnv) -->
     ir_merge_if_mods(PreEnv, Label2, Mod2, Label1, _{}, NewEnv).
-
+*/
 %%%%%%%%%%%%%%%%%%%
-%%% EXPRESSIONS %%%
+%%% ir %%%
 %%%%%%%%%%%%%%%%%%%
 
-ir_exps(ConstEnv, Env, L, LL, NewEnv) --> dcg_foldl(ir_exp(ConstEnv), Env, L, LL, NewEnv).
+% EXPRESSIONS_exps(ConstEnv, Env, L, LL, NewEnv) --> dcg_foldl(ir_exp(ConstEnv), Env, L, LL, NewEnv).
 
-ir_exp(_ConstEnv, Env, int(I), I, Env) --> !, [].
-ir_exp(_ConstEnv, Env, var(_VarType, Id), Reg, Env.add_ask(Id, Reg)) --> !, [].
+ir_exp(_ConstEnv, int(I), I, Env) -->
+    ir_empty_env(Env).
+
+ir_exp(_ConstEnv, var(VarType, Id), Reg, Env) -->
+    ir_ask_env(Id, VarType - Reg, Env).
+
+/*
 ir_exp(_ConstEnv, Env, false, 0, Env) --> !, [].
 ir_exp(_ConstEnv, Env, true, 1, Env) --> !, [].
 ir_exp(ConstEnv, Env, str(Str), V, Env) -->
@@ -115,13 +145,17 @@ ir_exp(ConstEnv, Env, E, V, NewEnv) -->
     ir_exp(ConstEnv, Env, E1, V1, Env1),
     ir_exp(ConstEnv, Env1, E2, V2, NewEnv),
     [V = VV], !.
+*/
 
-ir_exp(ConstEnv, Env, E, V, NewEnv) -->
+ir_exp(ConstEnv, E, V, Env) -->
     { E =.. [Op, E1, E2], member(Op, [+,-,*,/,'%',<,>,'<=','>=']), VV =.. [Op, V1, V2] }, !,
-    ir_exp(ConstEnv, Env, E1, V1, Env1),
-    ir_exp(ConstEnv, Env1, E2, V2, NewEnv),
-    [V = VV].
+    ir_exp(ConstEnv, E1, V1, Env1),
+    ir_exp(ConstEnv, E2, V2, Env2),
+    [V = VV],
+    { Ask set_is Env1.ask + Env2.ask },
+    ir_ask_env(Ask, Env).
 
+/*
 ir_exp(ConstEnv, Env, Exp, V, NewEnv) -->
     ir_cond(ConstEnv, Env, Exp, True, False, NewEnv),
     
@@ -157,35 +191,41 @@ ir_cond(ConstEnv, Env, E1 '||' E2, LabTrue, LabFalse, NewEnv) -->
 ir_cond(ConstEnv, Env, Exp, LabTrue, LabFalse, NewEnv) -->
     ir_exp(ConstEnv, Env, Exp, V, NewEnv),
     [ if(V, LabTrue, LabFalse) ].
+*/
 %%%%%%%%%%%%%%%%%%
 %%% STATEMENTS %%%
 %%%%%%%%%%%%%%%%%%
 
-ir_stmts(ConstEnv, Env, Stmts, NewEnv) --> dcg_foldl(ir_stmt(ConstEnv), Env, Stmts, NewEnv).
+ir_stmts(_ConsEnv, [], Env) --> ir_empty_env(Env).
+ir_stmts(ConstEnv, [Stmt | Stmts], Env) -->
+    ir_stmt(ConstEnv, Stmt, Env1),
+    ir_stmts(ConstEnv, Stmts, Env2),
+    semicolon_merge(Env1, Env2, Env).
 
-ir_stmt(_ConstEnv, Env, skip, Env) --> [].
+ir_stmt(_ConstEnv, skip, Env) --> ir_empty_env(Env).
 
-ir_stmt(ConstEnv, InEnv, block(Stmts), NewEnv) -->
-    { TempEnv = InEnv.reset() },
-    ir_stmts(ConstEnv, TempEnv, Stmts, OutEnv),
-    { NewEnv = InEnv.add_ask(OutEnv.ask).add_mod_set(OutEnv.mod).put(last_block, OutEnv.last_block) }.
+ir_stmt(ConstEnv, block(Stmts), Env) -->
+    ir_stmts(ConstEnv, Stmts, StmtEnv),
+    { Env = StmtEnv.put(gen, _{}) }.
 
-ir_stmt(ConstEnv, InEnv, (Id : _VarType) = Exp, OutEnv) -->
-    ir_exp(ConstEnv, InEnv, Exp, V, ExpEnv),
-    { OutEnv = ExpEnv.add_mod(Id, V) }.
+ir_stmt(ConstEnv, (Id : VarType) = Exp, Env) -->
+    ir_exp(ConstEnv, Exp, V, ExpEnv),
+    { Env = ExpEnv.add_to(mod, Id, VarType - V) }.
 
-ir_stmt(_ConstEnv, Env, return, Env) --> [ret].
-ir_stmt(ConstEnv, Env, return(RetType, Exp), NewEnv) -->
-    ir_exp(ConstEnv, Env, Exp, V, NewEnv),
+ir_stmt(_ConstEnv, return, Env) --> [ret], ir_empty_env(Env).
+ir_stmt(ConstEnv, return(RetType, Exp), Env) -->
+    ir_exp(ConstEnv, Exp, V, Env),
     [ret(RetType, V)].
 
-ir_stmt(_ConstEnv, Env, decl(_, []), Env) --> [].
 
-ir_stmt(ConstEnv, Env, decl(Type, [ init(Id, Exp) | T ]), NewEnv) -->
-    ir_exp(ConstEnv, Env, Exp, V, ExpEnv),
-    ir_stmt(ConstEnv, ExpEnv.add_var(Id, Type).add_create(Id, V), decl(Type,T), NewEnv).
+ir_stmt(_ConstEnv, decl(_Type, []), Env) --> ir_empty_env(Env).
 
+ir_stmt(ConstEnv, decl(Type, [ init(Id, Exp) | T ]), Env) -->
+    ir_exp(ConstEnv, Exp, V, ExpEnv),
+    ir_stmt(ConstEnv, decl(Type,T), StmtEnv),
+    semicolon_merge(ExpEnv.add_to(gen, Id,Type - V), StmtEnv, Env).
 
+/*
 ir_stmt(ConstEnv, Env, expstmt(Exp), NewEnv) -->
     ir_exp(ConstEnv, Env, Exp, _, NewEnv).
 
@@ -227,12 +267,12 @@ ir_stmt(ConstEnv, Env, while(While, Do), NewEnv) -->
 % ir_stmt(S) --> { writeln(S), fail }.
 
 ir_block(_ConstEnv, Env, Label, Env.put(last_block, Label)) --> [ block(Label) ].
+*/
 
 
-
-ir_fun_body(ConstEnv, FunEnv, Body) -->
-    [ block(StartBlock) ],
-    ir_stmts(ConstEnv, FunEnv.put(last_block,StartBlock), Body, _),
+ir_fun_body(ConstEnv, Body, Env) -->
+    [ block(_StartBlock) ],
+    ir_stmts(ConstEnv, Body, Env),
     % last block can be empty due to returns in bramches.
     [ unreachable ].
 
@@ -249,10 +289,8 @@ ir_args([(Id,Type) | T], SS, [(Reg,Type) | TT]) :-
 ir_fun(ConstEnv, topdef(Ret, Fun, Args, Body)) -->
     % { format(user_error, "compiling function: ~w : ~w -> ~w~n", [Fun, Args, Ret]) },
     {
-        ir_env(Env),
-        ir_args(Args, Mod, NArgs),
-        FunEnv = Env.add_mod_set(Mod),
-        phrase(ir_fun_body(ConstEnv, FunEnv, Body), Code)
+        ir_args(Args, _Mod, NArgs),
+        phrase(ir_fun_body(ConstEnv, Body, _Env), Code)
     },
     [ function(Ret, Fun, NArgs, Code) ].
 
