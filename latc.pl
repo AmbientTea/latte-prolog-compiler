9992a9cc065f19ba50cb9923b13cb4af69d21d87
Nikolaos Dymitriadis <nndymitriadis@gmail.com>
2017-01-06 00:59:23 +0100

    removed obsolete interpreter

--- latc.pl
+++ latc.pl
@@ -5,7 +5,6 @@
 :- use_module(utils).
 :- use_module(parser).
 :- use_module(frontend).
-:- use_module(eval).
 
 :- use_module(compile).
 
@@ -13,21 +12,7 @@
 :- % main
 (
 	OptSpec =
-		[ [ opt(content)
-		  , type(atom)
-		  , default(program)
-		  , shortflags([t])
-		  , longflags(['content'] )
-		  , help([ 'parsed content: exp, prog'])
-		  ]
-		, [ opt(mode)
-		  , type(atom)
-		  , default(compile)
-		  , shortflags([m])
-		  , longflags(['mode'])
-		  , help(['mode: eval, compile'])
-		  ]
-		, [ opt(out)
+		[ [ opt(out)
 		  , type(string)
 		  , default("")
 		  , shortflags([o])
@@ -44,24 +29,18 @@
     	],
 	current_prolog_flag(argv, LineArgs),
 	opt_parse(OptSpec, LineArgs, Opts, Args),
-	%member(content(Cont), Opts),
-	member(mode(Mode), Opts),
-	
+
 	(Args = [File | _]       -> true ; fail("no file specified")),
 	( member(out(Out), Opts), Out \= ""
 	; file_name_extension(Base, _, File), file_name_extension(Base, ".out", Out)),
+
 	(parse(Cont, File, Tree) -> true ; fail("parsing failed") ),
+
 	(check(Cont, Tree, Env, NTree) /*-> true ; fail("type check failed")*/),
 
-	( Mode = eval ->
-	    ( Cont = exp -> eval_exp(NTree)
-	    ; Cont = stmt -> eval_stmt(NTree)
-	    ; Cont = program -> eval_program(NTree) )
-    ; Mode = compile ->
-        ( compile(Opts, Env, NTree, Code),
-            writeln(Code)
-        ; fail("compilation error or some features not implemented yet"))
-    ; fail("invalid mode: ~w", [Mode])),
+    ( compile(Opts, Env, NTree, Code), writeln(Code)
+    ; fail("compilation error or some features not implemented yet")),
+	
 	halt(0)
 ) ; /* writeln("something went wrong"), */ halt(-1).
 
