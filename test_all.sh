a244228701672f76846b42c5b1743ca6a2ce6d4a
Nikolaos Dymitriadis <nndymitriadis@gmail.com>
2017-01-06 17:25:26 +0100

    better testing output

--- test_all.sh
+++ test_all.sh
@@ -2,26 +2,18 @@
 
 echo ========== testing frontend: =============
 for file in `ls bad/*.lat`; do
-    echo -n $file ... 
-    ./latc.pl $file
+    echo $file ...  `./latc.pl $file 2>&1 >/dev/null | head -n2 | tr "\n" "  "`
 done;
-
-if [ "$1" == "interpreter" ] || [ "$1" == "all" ]; then
-    echo ========== testint interpreter: ============
-    for file in `ls good/*.lat`; do
-        out=good/`basename $file .lat`.output
-        O=`./latc.pl -m eval $file | diff -s -q - $out`
-        if [ $? != 0 ]; then exit; fi
-	    echo $file ... $O
-    done;
-fi;
 
 if [ "$1" == "" ] || [ "$1" == "compiler" ] || [ "$1" == "all" ]; then
     echo ========== testing compiler: =============
     for file in `ls good/*.lat`; do
         out=good/`basename $file .lat`.output
-        PROG=`./latc $file`
+        echo -n "compile $file ... "
+        
+        PROG=`./latc $file 2> >(head -n 1 | tr -d "\n" 1>&2 )`
         if [ $? != 0 ]; then exit; fi
-        echo compiled $file ... `$PROG | diff -s -q - $out`
+        
+        echo " ..." `$PROG | diff -s -q - $out`
     done;
 fi;
