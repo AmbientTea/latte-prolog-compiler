8e8e12eef767b0fb0e06e64d59ead6f5f57ed79d
Nikolaos Dymitriadis <nndymitriadis@gmail.com>
2017-01-07 18:49:38 +0100

    moved tests to tests/ directory

--- test_all.sh
+++ test_all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo ========== testing frontend: =============
-for file in `ls bad/*.lat`; do
+for file in `ls tests/bad/*.lat`; do
     echo $file ...  `./latc $file 2>&1 >/dev/null | head -n2 | tr "\n" "  "`
 done;
 
 if [ "$1" == "" ] || [ "$1" == "compiler" ] || [ "$1" == "all" ]; then
     echo ========== testing compiler: =============
-    for file in `ls good/*.lat`; do
-        out=good/`basename $file .lat`.output
+    for file in `ls tests/good/*.lat`; do
+        out=`dirname $file`/`basename $file .lat`.output
         echo -n "compile $file ... "
         
         PROG=`./latc $file 2> >(head -n 1 | tr -d "\n" 1>&2 )`
