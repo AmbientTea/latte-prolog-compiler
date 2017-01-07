9a0420353ce0171785b6c040cd232610f594316a
Nikolaos Dymitriadis <nndymitriadis@gmail.com>
2017-01-07 19:21:24 +0100

    better testing, plugged in some more

--- test_all.sh
+++ test_all.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
-echo ========== testing frontend: =============
-for file in `ls tests/bad/*.lat`; do
+function test_bad() {
+    DIR=$1;
+    HEADER=$2;
+    
+    echo $HEADER:
+    for file in `ls $DIR/*.lat`; do
     echo $file ...  `./latc $file 2>&1 >/dev/null | head -n2 | tr "\n" "  "`
-done;
+    done;
+}
 
-if [ "$1" == "" ] || [ "$1" == "compiler" ] || [ "$1" == "all" ]; then
-    echo ========== testing compiler: =============
-    for file in `ls tests/good/*.lat`; do
+function test_good() {
+    DIR=$1;
+    HEADER=$2;
+    
+    echo $HEADER:
+    
+    
+    for file in `ls $DIR/*.lat`; do
         out=`dirname $file`/`basename $file .lat`.output
         echo -n "compile $file ... "
         
         PROG=`./latc $file 2> >(head -n 1 | tr -d "\n" 1>&2 )`
         if [ $? != 0 ]; then exit; fi
         
-        echo " ..." `$PROG | diff -s -q - $out`
+        echo " ..." `$PROG | diff -s -q - $out | grep -o "\(differ\|identical\)"`
     done;
-fi;
+}
+
+echo ========== FRONTEND TESTS: =============
+test_bad "tests/bad/" "BASIC"
+
+echo ========== COMPILER TESTS: =============
+test_good "tests/good/" "BASIC"
+test_good "tests/mrjp-tests/good/basic" "COMMUNITY BASIC"
+
+
